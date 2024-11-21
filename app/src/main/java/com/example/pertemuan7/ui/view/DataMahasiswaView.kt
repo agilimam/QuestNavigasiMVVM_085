b99221204a7c08f2319a6b3635350f751ec6dea6
agilimam <agilimamalhafizi04@gmail.com>
2024-11-21 21:33:16 +0700

    Menambahkan teks 'Tampilan Data dan parameter onclickbutton

--- app/src/main/java/com/example/pertemuan7/ui/view/DataMahasiswaView.kt
+++ app/src/main/java/com/example/pertemuan7/ui/view/DataMahasiswaView.kt
@@ -1,28 +1,71 @@
 package com.example.pertemuan7.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import model.DataMahasiswa
 
 @Composable
 fun DataMahasiswaView(
     modifier: Modifier = Modifier,
     uiStateMahasiswa: DataMahasiswa,
+    onClikButton: () -> Boolean
 ){
-    val listDataMahasiswa = listOf(
+    val listDataMhs = listOf(
         Pair("Nama", uiStateMahasiswa.nama),
         Pair("NIM", uiStateMahasiswa.nim),
         Pair("gender", uiStateMahasiswa.gender),
         Pair("Alamat", uiStateMahasiswa.alamat)
     )
 
-    Column {
-        listDataMahasiswa.forEach{items ->
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Tampilan Data",
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+
+        Spacer(Modifier.padding(16.dp))
+
+        listDataMhs.forEach{items ->
             CardSection(
                 judulParam = items.first,
                 Isiparams  = items.second
             )
         }
+        Button(onClick = {onClikButton()}) {
+            Text(text = "Kembali")
+        }
+    }
+}
+@Composable
+fun CardSection(judulParam : String, Isiparams : String){
+    Column(
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = judulParam, modifier = Modifier.weight(0.8f))
+            Text(text = ":", modifier = Modifier.weight(0.2f))
+            Text(text = "$Isiparams", modifier = Modifier.weight(2f))
+        }
+
     }
 }
